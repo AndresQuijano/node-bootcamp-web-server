118a1a512a982487855de1b1b54d47fe2a0fc736
Andres Quijano <andres.quijano@pragma.com.co>
2021-09-11 15:55:56 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
-    const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
     const messageOne = document.querySelector('#message-1');
     const messageTwo = document.querySelector('#message-2');
 
